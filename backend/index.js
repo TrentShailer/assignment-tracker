a2a77b7f2533049cb433feac3ee5de67a8b3a07f
Trent Shailer <trent.shailer@gmail.com>
2022-05-05 17:37:06 +1200

    Fastify webserver setup - removed typescript

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
-import Fastify from "fastify";
-import mainRoute from "./mainRoute";
-import fastifyStatic from "@fastify/static";
-import fastifySession from "@fastify/session";
-import fastifyCookie from "@fastify/cookie";
-import fastifyPostgres from "@fastify/postgres";
-import "dotenv/config";
-import path from "path";
-
+require("dotenv").config();
+const Fastify = require("fastify");
+const fastifyStatic = require("@fastify/static");
+const fastifySession = require("@fastify/session");
+const fastifyCookie = require("@fastify/cookie");
+const fastifyPostgres = require("@fastify/postgres");
+const path = require("path");
 const fastify = Fastify({
 	logger: true,
 });
 
 fastify.register(fastifyCookie);
+
 if (process.env.COOKIE_SECRET)
-	fastify.register(fastifySession, { secret: process.env.COOKIE_SECRET });
+	fastify.register(fastifySession, {
+		cookieName: "sessionId",
+		secret: process.env.COOKIE_SECRET,
+	});
 
 fastify.register(fastifyPostgres, {
 	connectionString: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
@@ -21,17 +23,20 @@ fastify.register(fastifyPostgres, {
 
 fastify.register(fastifyStatic, { root: path.join(__dirname, "build") });
 
-fastify.register(mainRoute);
+fastify.register(require("./pageRoute"));
 
 const start = async () => {
 	try {
 		await fastify.listen(2005);
-		/* const client = await fastify.pg.connect();
+		/*
+		const client = await fastify.pg.connect();
 		const someResult = (
 			await client.query<{ user_id: string; username: string; password: string }>(
-				"SELECT * FROM users;"
+				"SELECT * FROM users WHERE username = $1;", [request.body.username]
 			)
-		).rows; */
+		).rows;
+		client.release();
+		*/
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
--- backend/mainRoute.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-async function routes(fastify: FastifyInstance, options: any) {
-	fastify.get("/", async (request, reply) => {
-		return reply.sendFile("index.html");
-	});
-}
-
-export default routes;

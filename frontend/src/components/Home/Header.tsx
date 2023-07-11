cd0b8c70a15659bd62f4d45a977c4baed51ae686
Trent Shailer <trent.shailer@gmail.com>
2023-07-11 19:21:25 +1200

    Fix enter not submitting

--- frontend/src/components/Home/Header.tsx
+++ frontend/src/components/Home/Header.tsx
@@ -8,40 +8,39 @@ import CreateCourse from "./Header/CreateCourse";
 import Reload from "./Header/Reload";
 
 interface Props {
-  user: User;
-  SetUser: (user: User | null) => void;
-  FetchData: () => {};
+	user: User;
+	SetUser: (user: User | null) => void;
+	FetchData: () => {};
 }
 
 const NowString = (): string => {
-  return dayjs().format("h:mm a [on] dddd [the] Do [of] MMMM");
+	return dayjs().format("h:mm a [on] dddd [the] Do [of] MMMM");
 };
 
 export default function Header({ user, SetUser, FetchData }: Props) {
-  const [now, setNow] = useState(NowString());
+	const [now, setNow] = useState(NowString());
 
-  useEffect(() => {
-    setInterval(() => {
-      setNow(NowString());
-    }, 1000 * 60);
-  }, []);
-  return (
-    <Flex
-      background="whiteAlpha.100"
-      h="60px"
-      alignItems="center"
-      justifyContent="space-between"
-      gap={4}
-      p={4}
-    >
-      <Heading size="md" mt={-1} lineHeight="normal">
-        {now}
-      </Heading>
-      <Stack direction="row" gap={4}>
-        <CreateCourse FetchData={FetchData} />
-        <Reload FetchData={FetchData} />
-        <LogoutButton SetUser={SetUser} />
-      </Stack>
-    </Flex>
-  );
+	useEffect(() => {
+		setInterval(() => {
+			setNow(NowString());
+		}, 1000 * 60);
+	}, []);
+	return (
+		<Flex
+			background="whiteAlpha.100"
+			h="60px"
+			alignItems="center"
+			justifyContent="space-between"
+			gap={4}
+			p={4}>
+			<Heading size="md" mt={-1} lineHeight="normal" noOfLines={1}>
+				{now}
+			</Heading>
+			<Stack direction="row" gap={4}>
+				<CreateCourse FetchData={FetchData} />
+				<Reload FetchData={FetchData} />
+				<LogoutButton SetUser={SetUser} />
+			</Stack>
+		</Flex>
+	);
 }

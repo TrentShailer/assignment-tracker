61b70f9172e976b0243aeaa006018b736b6ddd5f
Trent Shailer <trent.shailer@gmail.com>
2024-01-15 17:16:17 +1300

    sqlx and typescript bindings

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -1,15 +1,18 @@
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
+use ts_rs::TS;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, TS)]
+#[ts(export)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
 }
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, TS)]
+#[ts(export)]
 pub struct Assignment {
     pub id: Uuid,
     pub course_id: Uuid,
@@ -19,7 +22,8 @@ pub struct Assignment {
     pub progress: i16,
 }
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, TS)]
+#[ts(export)]
 pub struct Course {
     pub id: Uuid,
     pub name: String,
